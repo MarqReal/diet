0fb4ae23357c9f8e905853a41b06ae51ef6b83b9
vagrant <vagrant@homestead>
2019-08-07 00:05:33 +0000

    Base de formulario de cadastro

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -3,5 +3,39 @@
 @section('titulo', 'Cursos')
 
 @section('conteudo')
-	<h3>Lista de Cursos</h3>
+	<div class="container">
+		<h3 class="center">Lista de Cursos</h3>
+		<div class="row">
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Titulo</th>
+						<th>Descrição</th>
+						<th>Imagem</th>
+						<th>Publicado</th>
+						<th>Ação</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($registros as $registro)
+					<tr>
+						<td>{{$registro->id}}</td>
+						<td>{{$registro->titulo}}</td>
+						<td>{{$registro->descricao}}</td>
+						<td><img width="120" src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"></td>
+						<td>{{$registro->publicado}}</td>
+						<td>
+							<a class="btn deep-orange" href="{{route('admin.cursos.editar', $registro->id)}}"></a>
+							<a class="btn red" href="{{route('admin.cursos.deletar', $registro->id)}}"></a>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+		<div class="row">
+			<a class="btn blue" href="{{route('admin.cursos.adicionar')}}"> Adicionar</a>
+		</div>
+	</div>	
 @endsection
