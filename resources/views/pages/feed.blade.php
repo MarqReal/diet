31a5aec3d55673817ddef7341d6906458dd6765c
vagrant <vagrant@homestead>
2019-08-19 18:02:26 +0000

    subindo login funcionando com data de nascimento e sair

--- resources/views/pages/feed.blade.php
+++ resources/views/pages/feed.blade.php
@@ -57,35 +57,37 @@
 	<script type="text/javascript">
 		$(document).ready(function () {
       		Materialize.updateTextFields();
-
-      		$("#btnEntrar").click(function () {
-      			if($("#email").val()== "" || $("#email").val().indexOf('@')== -1 || $("#email").val().indexOf('.')== -1) {
-      				Swal.fire('Preenchimento incorreto!', "Formato de E-mail incorreto",'error');
-	  				$("#email").focus();
-	  				return false;
-				}
-      			var data = {
-      				'_token': '{{csrf_token()}}',
-      				'email' : $("#email").val(),
-      				'senha' : $("#senha").val()
-      			};
-				$.ajax({
-    				method: 'POST', // Type of response and matches what we said in the route
-    				url: '/login/entrar', // This is the url we gave in the route
-    				data: data, // a JSON object to send back
-			    	success: function(response){ // What to do if we succeed
-						var resposta = JSON.parse(response);
-						if (!resposta.error) {
-							window.location.href = "/registro";
-						} else {
-							Swal.fire('Erro!', "E-mail ou senha invalidos, tente novamente",'error');
-						} 
-			    	},
-			    	error: function(jqXHR, textStatus, errorThrown) { // What to do if we fail
-			        	//console.log(JSON.stringify(jqXHR));
-						Swal.fire('Erro!', "Não foi possivel realizar o cadastro, tente novamente",'error');
+      		$("#sair").click(function () {
+      			Swal.fire({
+  					title: 'Você tem certeza que quer sair?',
+  					text: "",
+  					type: 'warning',
+  					showCancelButton: true,
+  					confirmButtonColor: '#3085d6',
+  					cancelButtonColor: '#d33',
+  					confirmButtonText: 'Sim',
+  					cancelButtonText : 'Cancelar'
+				}).then((result) => {
+  					if (result.value) {
+  						$.ajax({
+    						method: 'GET', // Type of response and matches what we said in the route
+    						url: '/login/sair', // This is the url we gave in the route
+    						data: {}, // a JSON object to send back
+			    			success: function(response){ // What to do if we succeed
+								var resposta = JSON.parse(response);
+								if (!resposta.error) {
+									window.location.href = "/registro";
+								} else {
+									Swal.fire('Erro!', "E-mail ou senha invalidos, tente novamente",'error');
+								} 
+			    			},
+			    			error: function(jqXHR, textStatus, errorThrown) { // What to do if we fail
+			        		//console.log(JSON.stringify(jqXHR));
+							Swal.fire('Erro!', "Não foi possivel realizar o cadastro, tente novamente",'error');
 			        	//console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
-			    	}
+			    			}
+				});
+  					}
 				});
       		});
       	});
