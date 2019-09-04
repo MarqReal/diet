3dbf705103d03a20c602aca4d915b47305b17dc7
vagrant <vagrant@homestead>
2019-09-04 00:17:05 +0000

    Relacionamento dieta e alimento funcionando

--- app/Dieta.php
+++ app/Dieta.php
@@ -11,11 +11,24 @@ class Dieta extends Model
     	$this->nome = $dados["nome"];
     	$this->objetivo = $dados["objetivo"];
     	$this->save();
+    	
     	foreach ($dados['cafeManha'] as $key => $alimento_id) {
     		$alimento = Alimento::find($alimento_id);
     		$this->alimentos()->save($alimento, ["refeicao" => "cafeManha", "quantidade" => $dados['qtdCafeManha'][$alimento->nome]]);
     	}
-    	
+    	foreach ($dados['cafeTarde'] as $key => $alimento_id) {
+    		$alimento = Alimento::find($alimento_id);
+    		$this->alimentos()->save($alimento, ["refeicao" => "cafeTarde", "quantidade" => $dados['qtdCafeTarde'][$alimento->nome]]);
+    	}
+    	foreach ($dados['almoco'] as $key => $alimento_id) {
+    		$alimento = Alimento::find($alimento_id);
+    		$this->alimentos()->save($alimento, ["refeicao" => "almoco", "quantidade" => $dados['qtdAlmoco'][$alimento->nome]]);
+    	}
+    	foreach ($dados['jantar'] as $key => $alimento_id) {
+    		$alimento = Alimento::find($alimento_id);
+    		$this->alimentos()->save($alimento, ["refeicao" => "jantar", "quantidade" => $dados['qtdJantar'][$alimento->nome]]);
+    	}
+
     }
 
     public function editar ($dados)
